837d1f0b62d77bfcff5054312306e810cdf24d2b
jenny <yn3524@gmail.com>
2018-06-09 03:32:02 +0900

    sfefw

--- LMP/src/app/result/result.component.ts
+++ LMP/src/app/result/result.component.ts
@@ -5,8 +5,6 @@ import { Location } from '@angular/common';
 import { CheckList } from '../checklist';
 import { ClService } from '../cl.service';
 
-import { AngularFirestore } from 'angularfire2/firestore';
-
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -21,13 +19,8 @@ export class ResultComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private clservice: ClService,
-    private location: Location,
-    private db: AngularFirestore,
-  ) 
-  { 
-    db.firestore.settings({ timestampsInSnapshots: true });
-    db.firestore.enablePersistence();
-  }
+    private location: Location
+  ) { }
 
   ngOnInit() {
   }
@@ -42,7 +35,7 @@ export class ResultComponent implements OnInit {
   }
 
   getResultValue(){
-    /*var value = 0;
+    var value = 0;
     for(var i = 0; i < this.cl.questions.length; i++){
       var count = 0;
       for(var j = 0; j < this.cl.questions[i].checkboxes.length; j++){
@@ -53,8 +46,7 @@ export class ResultComponent implements OnInit {
       value += count/this.cl.questions[i].checkboxes.length;
     }
 
-    this.progressValue = (value/this.cl.questions.length)*100;*/
-    this.progressValue= 100;
+    this.progressValue = (value/this.cl.questions.length)*100;
     console.log(this.progressValue);
   }
 
